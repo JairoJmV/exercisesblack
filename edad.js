bc2c2be9cdf7dbabb35d7544b54c3f3b86bf2198
JJMV <jairoj.moralesv15@gmail.com>
2022-10-31 17:06:19 -0500

    newcommit

---
+++ edad.js
@@ -0,0 +1,11 @@
+//1. dado un usuario con nombre y edad, determinar si puede beber alcohol o si puede empezar a recibir pensión
+
+const edad = 62
+
+if(edad >= 18){
+    console.log('puedes beber')
+}
+else(console.log('eres un pinche menor'))
+if(edad >= 62){
+    console.log('y puedes empezar a recibir pensión')
+}
