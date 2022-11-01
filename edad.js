44face08ad7f83802066d96e3148459d05ff712c
JJMV <jairoj.moralesv15@gmail.com>
2022-11-01 10:55:50 -0500

    commit4

--- edad.js
+++ edad.js
@@ -1,7 +1,12 @@
 //1. dado un usuario con nombre y edad, determinar si puede beber alcohol o si puede empezar a recibir pensión
 
-const edad = 22
+/*
+const promp = require("prompt-sync");
 
+const nombre = promp("cual es tu nombre?");
+console.log(nombre);
+
+const edad = promp("cuantos años tienes?");
 if(edad >= 18){
     console.log('puedes beber')
 }
@@ -9,3 +14,35 @@ else(console.log('eres un pinche menor'))
 if(edad >= 62){
     console.log('y puedes empezar a recibir pensión')
 }
+*/
+
+
+const nombre = require("readline");
+const rl = nombre.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.question("cual es tu nombre?", function(answer){
+    console.log(answer);
+    rl.close();
+});
+
+const edad = require("readline");
+const ed = edad.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+ed.question("cuantos años tienes?", function(answer){
+    console.log('como tu edad es ' + answer);
+    ed.close();
+});
+
+if(edad >= 18){
+    console.log('puedes beber')
+    }
+else(console.log('eres un pinche menor'))
+if(edad >= 62){
+    console.log('y puedes empezar a recibir pensión')
+    }
