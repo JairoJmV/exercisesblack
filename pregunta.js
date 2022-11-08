3038d5aa463fa774861a26b398ee78ea1f8a255d
JJMV <jairoj.moralesv15@gmail.com>
2022-11-08 18:01:29 -0500

    commit5

--- pregunta.js
+++ pregunta.js
@@ -1,14 +1,11 @@
 //5. preguntar al usuario n veces una pregunta hasta que la respuesta sea correcta
 
-const pregunta = 'cuanto es 2+2 ';
+const pregunta = rp;
 const respuesta = require("readline");
 const rp = respuesta.createInterface({
     input: process.stdin,
     output: process.stdout,
 });
 rp.question(pregunta, function(answer){
-    if(answer == 4){
-        return('correcto' + rp.close())
-      } 
-    else(rp.question(pregunta)) 
+   
 });
