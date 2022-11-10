9e6db7bd0b1e89ed3a67a6822efeaf87c9309bfa
JJMV <jairoj.moralesv15@gmail.com>
2022-11-10 17:43:49 -0500

    commitvsc

--- notas.js
+++ notas.js
@@ -1,12 +1,12 @@
 //2. dado un arreglo de notas, determinar el promedio de todas las notas
-/*
+
 math = 2;
 sciences = 4;
 grammar = 3.5;
 gym = 4.3;
 music = 3;
-*/
 
-const notas = [2, 4, 3.5, 4.3, 3];
+
+const notas = [math, sciences, grammar, gym, music];
 const promedio = notas.reduce((a,b) => a + b, 0) / notas.length;
 console.log(promedio);
--- pregunta.js
+++ pregunta.js
@@ -1,5 +1,30 @@
 //5. preguntar al usuario n veces una pregunta hasta que la respuesta sea correcta
 
+const readLine = require("readline");
+
+const reader = readLine.createInterface({ input: process.stdin, output: process.stdout });
+
+const makeQuestion = async (question) => {
+  return new Promise((resolve) =>
+    reader.question(question, answer => {
+      resolve(answer)
+      reader.close();
+    })
+  )
+}
+
+(async () => {
+  while(true){
+    const response = await makeQuestion('Who\'s the best bass player? ');
+    if(response.toLowerCase() === 'kevin') {
+      console.log('You\'re right');
+      break;
+    }
+    console.log('Bad answer');
+  }
+})()
+
+/*
 const pregunta = rp;
 const respuesta = require("readline");
 const rp = respuesta.createInterface({
@@ -9,3 +34,5 @@ const rp = respuesta.createInterface({
 rp.question(pregunta, function(answer){
    
 });
+*/
+
