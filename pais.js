376b74d92382dd98c5ac2cb4cb1618df41bfe476
JJMV <jairoj.moralesv15@gmail.com>
2022-11-15 17:54:59 -0500

    commitvsc3

--- pais.js
+++ pais.js
@@ -1,3 +1,18 @@
 //4. dado un pais, imprimir el gentilicio mas el nombre del usuario
 
 const name = Jairo;
+const colombia = colombiano;
+const peru = peruano;
+const bolivia = boliviano;
+const readLine = require("readline");
+const country = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+country.question('choose your country : Colombia, Perú, Bolivia, Argentina', function(answer){
+    if(answer === colombia){
+        console.log(name + 'you are' + colombia)
+    }
+
+})
