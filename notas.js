e89161b72fdb7a8896a8cb89226b32341826b367
JDJMV <jairoj.moralesv15@gmail.com>
2023-01-19 21:21:29 -0500

    aprendiendo a pensar en iterador

--- notas.js
+++ notas.js
@@ -1,12 +1,12 @@
 //2. dado un arreglo de notas, determinar el promedio de todas las notas
+const notas = [2, 4, 5]
+let promedio = 0
 
-math = 2;
-sciences = 4;
-grammar = 3.5;
-gym = 4.3;
-music = 3;
+for (let index = 0; index < notas.length; index++) {
+    promedio = promedio + notas[index]
+}
 
+console.log(promedio / notas.length)
 
-const notas = [math, sciences, grammar, gym, music];
-const promedio = notas.reduce((a,b) => a + b, 0) / notas.length;
-console.log(promedio);
+// arreglos con 0 y 1 ¿hay mas 0 o hay mas 1?
+
