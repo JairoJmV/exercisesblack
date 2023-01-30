1f52b603fe4843029fade2a24141834f893cf572
JJMV <jairoj.moralesv15@gmail.com>
2023-01-30 17:55:58 -0500

    arreglo con forEach

--- arreglo.js
+++ arreglo.js
@@ -4,7 +4,22 @@ let numCero = 1
 
 for (let index = 0; index < arreglo.length; index++) {
     numUno = numUno + arreglo[index];
+    //numCero = arreglo.length[index = 0] + 1;
+}
+console.log(numUno);
+console.log(numCero);
+
+/*
+for (let index = 0; index < arreglo.length; index++) {
+    numUno = numUno + arreglo[index];
     numCero = numCero + arreglo[index];
 }
 console.log(numUno);
-console.log(numCero)
+console.log(numCero);
+
+arreglo.forEach(element =>  {
+    arreglo.length[index] = 1, - 1;
+    arreglo.length[index] = 0, + 1;
+});
+console.log(element);
+*/
