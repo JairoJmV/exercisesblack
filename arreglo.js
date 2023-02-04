7291fb00be53c573428fb9f9c7f0e642a85b1c2b
JJMV <jairoj.moralesv15@gmail.com>
2023-02-04 09:17:49 -0500

    resolviendo el arreglo con if

--- arreglo.js
+++ arreglo.js
@@ -1,25 +1,14 @@
 const arreglo = [0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0]
 let numUno = 0
-let numCero = 1
+let numCero = 0
 
 for (let index = 0; index < arreglo.length; index++) {
-    numUno = numUno + arreglo[index];
-    //numCero = arreglo.length[index = 0] + 1;
+    if(arreglo[index] === 0){
+        numCero++;
+    }if(arreglo[index] === 1){
+        numUno++;
+    }
 }
 console.log(numUno);
 console.log(numCero);
 
-/*
-for (let index = 0; index < arreglo.length; index++) {
-    numUno = numUno + arreglo[index];
-    numCero = numCero + arreglo[index];
-}
-console.log(numUno);
-console.log(numCero);
-
-arreglo.forEach(element =>  {
-    arreglo.length[index] = 1, - 1;
-    arreglo.length[index] = 0, + 1;
-});
-console.log(element);
-*/
