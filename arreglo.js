70aeb3d4736cb58e63a889b553a9162a9e237ac5
JJMV <jairoj.moralesv15@gmail.com>
2023-02-08 15:56:44 -0500

    Arreglo con while

--- arreglo.js
+++ arreglo.js
@@ -1,3 +1,21 @@
+//Arreglo con While
+const arreglo = [0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0]
+let numUno = 0
+let numCero = 0
+let index = 0
+
+while(index < arreglo.length){
+    if(arreglo[index] === 0){
+        numCero++;
+    }if(arreglo[index] === 1){
+        numUno++;
+    }
+    index++;
+}
+console.log(numUno);
+console.log(numCero);
+
+/* Arreglo con For
 const arreglo = [0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0]
 let numUno = 0
 let numCero = 0
@@ -11,4 +29,4 @@ for (let index = 0; index < arreglo.length; index++) {
 }
 console.log(numUno);
 console.log(numCero);
-
+*/
