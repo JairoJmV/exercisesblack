fa60fbcf674f7b64927b130238db6b115d53f570
JJMV <jairoj.moralesv15@gmail.com>
2023-03-18 13:05:32 -0500

    *-

--- forwhile.js
+++ forwhile.js
@@ -1,11 +1,11 @@
-// ex: const = 3 imprimir * ** *** / // /// 
+// ex: const = 3 imprimir */**//***///
+const caracter = [3];
 
-const caracter = 3;
-let asterisco = "* ";
-let slash = "/ ";
 
-for (let index = 0; index < caracter; index++) {
-   
+for (let index = 1; index < caracter; index++) {
+    if (index <= caracter) {
+        console.log('*' * 2)
+    }
 };
 
 
